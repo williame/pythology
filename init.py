8b7d6ae30152bdf50f464ec9fac077feb04523c5
Rajeev N Bharshetty <coder.rshetty@gmail.com>
2012-04-11 12:33:04 +0530

    Indentation from 8 to 4 spaces (Suggestion by kracekumar) Thanks

--- init.py
+++ init.py
@@ -2,12 +2,12 @@
 import sys
 
 def main():
-	n = 10 
-	print n
-	def add(): ##Function in python starts with def keyword
-		n+10
+    n = 10 
+    print n
+    def add(): ##Function in python starts with def keyword
+        n+10
 	print n
 
 
 if __name__ == '__main__':
-  main()
+    main()
