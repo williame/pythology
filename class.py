6b4e14f246d6eac6caa69c63f0f9ed5c0ab2e535
Rajeev N Bharshetty <coder.rshetty@gmail.com>
2012-04-11 12:31:04 +0530

    Indentation changed from 8 to 4 spaces .

--- class.py
+++ class.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 class Test:
-	def __init__(self,a,b):
-		self.a=a;
-		self.b=b;
+    def __init__(self,a,b):
+        self.a=a;
+	self.b=b;
 	
 def main():
-	x = Test(10,3)
-	print x.a
-	print x.b
+    x = Test(10,3)
+    print x.a
+    print x.b
 
 if __name__ == '__main__':
-	main()
+    main()
 	
