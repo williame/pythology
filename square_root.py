bf84ee809a17b24f27f78af18a3edb93c04141d1
williame <varfar@yahoo.co.uk>
2012-04-12 10:42:43 +0300

    just playing with the scoping of the functions; there's no reason for it all to be global

--- square_root.py
+++ square_root.py
@@ -1,30 +1,18 @@
-from math import *
 
-def main():
-    x = int(raw_input("Enter a number to get the square root"))
-    guess = 1
-    guess = improve_answer(guess,x)
-    square_root(guess,x)
-
-def square_root(guess,x):
-    while(not good_enough(guess,x)):
-        guess = improve_answer(guess,x)
-    print guess
-
-def average(a,b):
-    return (a+b)/2.0
-
-def improve_answer(guess,x):
-    return average(guess,x/guess)
-
-def good_enough(guess,x):
-    good = abs(guess*guess-x)
-    return(good<0.001)
-    
+def square_root(x,guess=1):
+    def improve_answer():
+        def average(a,b):
+            return (a+b)/2.0
+        return average(guess,x/guess)   
+    def good_enough():
+        good = abs(guess*guess-x)
+        return good<0.001
+    while not good_enough():
+        guess = improve_answer()
+    return guess
 
 if __name__ == '__main__':
-    main()
-
-
-
-
+    x = float(raw_input("Enter a number to get the square root: "))
+    sqrt = square_root(x)
+    print "sqrt(%f) = %f"%(x,sqrt)
+    print "sqrd(%f) = %f"%(sqrt,sqrt*sqrt)
